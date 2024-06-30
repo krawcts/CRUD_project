d833267d399e712eaa023856e1def968d46a0a0e
krawcts <lucassampaio22k@gmail.com>
2024-06-29 23:03:55 -0300

    create get routes

--- backend/router.py
+++ backend/router.py
@@ -10,3 +10,28 @@ from crud import (
     delete_product,
     update_product,
 )
+
+router = APIRouter()
+
+# create route to fetch all itens
+@router.get("/products/", response_model=List[ProductResponse])
+def read_all_products(db: Session = Depends(get_db)):
+    all_products = get_all_products(db)
+    return all_products
+
+# create route to fetch 1 item
+@router.get("/products/{product_id}", response_model=ProductResponse)
+def read_one_product(product_id: int, db: Session = Depends(get_db)):
+    product = get_product(db=db, product_id=product_id)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product does not exist.")
+    return product
+
+# create route to add 1 item
+@router.post()
+
+# create route to delete 1 item
+@router.delete()
+
+# create route to update 1 item
+@router.put()
