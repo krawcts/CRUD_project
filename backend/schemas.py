d4b210c8540e4ae2162cd2047763cc3f41f4592b
krawcts <lucassampaio22k@gmail.com>
2024-06-30 01:55:38 -0300

    create category validation

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel, PositiveFloat, EmailStr, field_validator, Field
+from pydantic import BaseModel, PositiveFloat, EmailStr, field_validator
 from enum import Enum
 from datetime import datetime
 from typing import Optional
+
+class CategoryBase(Enum):
+    category1 = "Eletronics"
+    category2 = "Household Appliance"
+    category3 = "Furnite"
+    category4 = "Clothes"
+    category5 = "Shoes"
 
 class ProductBase(BaseModel):
     name: str
@@ -9,6 +16,12 @@ class ProductBase(BaseModel):
     price: PositiveFloat
     category: str
     supplier_email: EmailStr
+
+    @field_validator("category")
+    def check_category(cls, v):
+        if v in [item.value for item in CategoryBase]:
+            return v
+        raise ValueError("Invalid category")
 
 class ProductCreate(ProductBase):
     pass
@@ -26,3 +39,11 @@ class ProductUpdate(BaseModel):
     price: Optional[PositiveFloat] = None
     category: Optional[str] = None
     supplier_email: Optional[EmailStr] = None
+
+    @field_validator("category", pre=True, always=True)
+    def check_category(cls, v):
+        if v is None:
+            return v
+        if v in [item.value for item in CategoryBase]:
+            return v
+        raise ValueError("Invalid category")
